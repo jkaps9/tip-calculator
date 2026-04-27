02f4f98f34153e279358b7f23c3439131bd7742b
Josh <15219491+jkaps9@users.noreply.github.com>
2026-04-26 23:12:46 -0400

    set to a basic astro template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://example.com", //TODO: update me!
-  integrations: [
-    sitemap({
-      filter: (page) => !page.includes("/admin"),
-      changefreq: "weekly",
-      priority: 0.7,
-    }),
-  ],
-});
+export default defineConfig({});
--- src/scripts/menu.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-
-navToggle?.addEventListener("click", () => {
-  const isExpanded = navToggle.getAttribute("aria-expanded") === "true";
-  navToggle.setAttribute("aria-expanded", `${!isExpanded}`);
-});
