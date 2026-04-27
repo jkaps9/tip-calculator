9c41b435cd603b6b65ca80510f21426ed6efa411
Josh <15219491+jkaps9@users.noreply.github.com>
2026-04-27 00:48:51 -0400

    add functionality to reset button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,15 @@
 const tipButtons = document.querySelectorAll(".tip-btn");
+const inputBill = document.querySelector("input#bill");
+const inputCustomTip = document.querySelector("input#customTip");
+const inputNumPeople = document.querySelector("input#numPeople");
+const resetButton = document.querySelector(".reset-btn");
 
-if (tipButtons) {
-  tipButtons.forEach((tipButton) => {
-    tipButton.addEventListener("click", () => {
-      toggleClass(tipButtons, tipButton, "active");
-    });
-  });
-}
+const tipPerPerson = document.querySelector("#tipPerPerson");
+const totalPerPerson = document.querySelector("#totalPerPerson");
+
+let tipPercentage = 0;
+let billAmount = 0;
+let numPeople = 0;
 
 function toggleClass(list, element, className) {
   list.forEach((item) => {
@@ -15,3 +18,30 @@ function toggleClass(list, element, className) {
 
   element.classList.add(className);
 }
+
+function setTipPercentage(percentage) {
+  tipPercentage = percentage;
+}
+
+function calculateTip() {
+  if (billAmount > 0) {
+    return billAmount * (tipPercentage / 100);
+  } else {
+    return -1;
+  }
+}
+
+function setTotals(tipAmount, totalAmount) {
+  tipPerPerson.textContent = `$${tipAmount.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+  totalPerPerson.textContent = `$${totalAmount.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+}
+
+tipButtons?.forEach((tipButton) => {
+  tipButton.addEventListener("click", () => {
+    toggleClass(tipButtons, tipButton, "active");
+  });
+});
+
+resetButton?.addEventListener("click", () => {
+  setTotals(0, 0);
+});
